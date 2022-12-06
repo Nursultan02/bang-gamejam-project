c39e617a9c722f06f749bc7a517878af9ad0742a
bekdaulet <bekdauletarynbek@gmail.com>
2022-12-06 18:05:58 +0600

    fix bug

---
+++ src/components/text.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="text-blue-200"> Hello WOrlds</div>
+</template>
+
+<script>
+export default {
+  name: "textCustom",
+  props: {
+    text: String,
+  },
+  mounted() {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
