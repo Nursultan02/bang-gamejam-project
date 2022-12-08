a3e37cb6200970534236e6d19cbab76434527812
bekdaulet <bekdauletarynbek@gmail.com>
2022-12-09 05:27:51 +0600

    optimization test

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,13 @@ const VuePreloadPlugin = require('@vue/preload-webpack-plugin')
 
 const myPreloadPlug = new VuePreloadPlugin({
   rel: 'preload',
-  fileBlacklist: [/\.js/]
+  fileBlacklist: [/\.js/],
+  as(entry) {
+    if (/\.css$/.test(entry)) return 'style';
+    if (/\.woff$/.test(entry)) return 'font';
+    if (/\.png$/.test(entry)) return 'image';
+    return 'script';
+  }
 })
 module.exports = defineConfig({
   transpileDependencies: true,
